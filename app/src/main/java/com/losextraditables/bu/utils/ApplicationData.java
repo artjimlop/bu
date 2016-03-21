1e1b6f1ad77ca1735d2f97d503cfa511d5ba596d
Arturo <artjimlop@gmail.com>
2016-03-21 11:44:30 +0100

    Redirect uri added to the properties file
    It's needed in order to do the instagram login properly. Added to the properties file, obtained in Gradle and added to BuildConfig

--- app/src/main/java/com/losextraditables/bu/utils/ApplicationData.java
+++ app/src/main/java/com/losextraditables/bu/utils/ApplicationData.java
@@ -5,5 +5,5 @@ import com.losextraditables.bu.BuildConfig;
 public class ApplicationData {
 	public static final String CLIENT_ID = BuildConfig.CLIENT_ID;
 	public static final String CLIENT_SECRET = BuildConfig.CLIENT_SECRET;
-	public static final String CALLBACK_URL = "instagram://connect";
+	public static final String CALLBACK_URL = BuildConfig.REDIRECT_URI;
 }
