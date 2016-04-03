927ed2538eff4771148e0af014eef1a714f2a08b
Arturo <artjimlop@gmail.com>
2016-04-03 13:42:05 +0200

    Presenter covered with tests

--- app/src/main/java/com/losextraditables/bu/instagrammers/view/presenter/SearchInstagrammersPresenter.java
+++ app/src/main/java/com/losextraditables/bu/instagrammers/view/presenter/SearchInstagrammersPresenter.java
@@ -1,7 +1,5 @@
 package com.losextraditables.bu.instagrammers.view.presenter;
 
-import android.util.Log;
-
 import com.karumi.rosie.domain.usecase.UseCaseHandler;
 import com.karumi.rosie.domain.usecase.annotation.Success;
 import com.karumi.rosie.domain.usecase.callback.OnSuccessCallback;
@@ -9,12 +7,9 @@ import com.karumi.rosie.domain.usecase.error.OnErrorCallback;
 import com.losextraditables.bu.base.view.presenter.BuPresenter;
 import com.losextraditables.bu.instagrammers.domain.model.SearchedInstagrammer;
 import com.losextraditables.bu.instagrammers.domain.usecase.SearchInstagrammers;
-import com.losextraditables.bu.instagrammers.view.model.InstagrammerModel;
 import com.losextraditables.bu.instagrammers.view.model.SearchedInstagrammerModel;
-import com.losextraditables.bu.instagrammers.view.model.mapper.InstagrammerModelMapper;
 import com.losextraditables.bu.instagrammers.view.model.mapper.SearchedInstagrammerModelMapper;
 
-import java.util.Collection;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -22,20 +17,15 @@ import javax.inject.Inject;
 public class SearchInstagrammersPresenter extends BuPresenter<SearchInstagrammersPresenter.View> {
 
     private final SearchInstagrammers searchInstagrammers;
-    private final InstagrammerModelMapper instagrammerModelMapper;
     private final SearchedInstagrammerModelMapper searchedInstagrammerModelMapper;
-    private String query;
 
-    @Inject
-    public SearchInstagrammersPresenter(UseCaseHandler useCaseHandler, SearchInstagrammers searchInstagrammers, InstagrammerModelMapper instagrammerModelMapper, SearchedInstagrammerModelMapper searchedInstagrammerModelMapper) {
+    @Inject public SearchInstagrammersPresenter(UseCaseHandler useCaseHandler, SearchInstagrammers searchInstagrammers, SearchedInstagrammerModelMapper searchedInstagrammerModelMapper) {
         super(useCaseHandler);
         this.searchInstagrammers = searchInstagrammers;
-        this.instagrammerModelMapper = instagrammerModelMapper;
         this.searchedInstagrammerModelMapper = searchedInstagrammerModelMapper;
     }
 
     public void search(String query, String accessToken) {
-        this.query = query;
         getView().hideEmpty();
         getView().hideContent();
         getView().hideKeyboard();
@@ -44,19 +34,26 @@ public class SearchInstagrammersPresenter extends BuPresenter<SearchInstagrammer
         createUseCaseCall(searchInstagrammers).args(query, accessToken).onSuccess(new OnSuccessCallback() {
             @Success
             public void onInstagrammersFound(List<SearchedInstagrammer> instagrammers) {
-                List<SearchedInstagrammerModel> models = searchedInstagrammerModelMapper.map(instagrammers);
-                Log.d("instagrammers", instagrammers.get(0).toString());
-                getView().showContent();
-                getView().renderInstagrammers(models);
+                showInstagrammersInView(instagrammers);
             }
         }).onError(new OnErrorCallback() {
             @Override
             public boolean onError(Error error) {
-                getView().showEmpty();
-                getView().showConnectionError();
+                showErrorInView();
                 return false;
             }
         }).execute();
+    }
+
+    protected void showErrorInView() {
+        getView().showEmpty();
+        getView().showConnectionError();
+    }
+
+    protected void showInstagrammersInView(List<SearchedInstagrammer> instagrammers) {
+        List<SearchedInstagrammerModel> models = searchedInstagrammerModelMapper.map(instagrammers);
+        getView().showContent();
+        getView().renderInstagrammers(models);
     }
 
     public interface View extends BuPresenter.View {
