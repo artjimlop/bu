a38e7089c7dfdf814bb067ffeee1fc4243f3a0f0
jmartinez360 <mrjem1360@gmail.com>
2016-04-14 17:26:54 +0200

    SignInActivity extends from BuAppCompatActivity
    Due that was not possible to change the statusbarColor when this
    activity extended from BuActivity, I had to change the extends for
    BuAppCompatActivity

--- app/src/main/java/com/losextraditables/bu/login/view/activity/SignInActivity.java
+++ app/src/main/java/com/losextraditables/bu/login/view/activity/SignInActivity.java
@@ -3,13 +3,16 @@ package com.losextraditables.bu.login.view.activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import butterknife.Bind;
 import butterknife.OnClick;
 import com.karumi.rosie.view.Presenter;
 import com.losextraditables.bu.R;
-import com.losextraditables.bu.base.view.activity.BuActivity;
+import com.losextraditables.bu.base.view.activity.BuAppCompatActivity;
 import com.losextraditables.bu.instagrammers.view.activity.InstagrammersListActivity;
 import com.losextraditables.bu.login.LoginModule;
 import com.losextraditables.bu.login.view.presenter.SignInPresenter;
@@ -17,63 +20,65 @@ import java.util.Collections;
 import java.util.List;
 import javax.inject.Inject;
 
-public class SignInActivity extends BuActivity implements SignInPresenter.View {
+public class SignInActivity extends BuAppCompatActivity implements SignInPresenter.View {
 
-  @Inject @Presenter
-  SignInPresenter presenter;
+    @Inject @Presenter SignInPresenter presenter;
 
-  @Bind(R.id.email)
-  AutoCompleteTextView email;
-  @Bind(R.id.password)
-  EditText password;
+    @Bind(R.id.email) AutoCompleteTextView email;
+    @Bind(R.id.password) EditText password;
 
-  @Override protected List<Object> getActivityScopeModules() {
-    return Collections.singletonList((Object) new LoginModule());
-  }
+    @Override protected List<Object> getActivityScopeModules() {
+        return Collections.singletonList((Object) new LoginModule());
+    }
 
-  @Override
-  protected int getLayoutId() {
-    return R.layout.activity_sign_in;
-  }
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupStatusBarColor();
+    }
 
-  @Override
-  protected void redirectToLogin() {
+    private void setupStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
 
-  }
+    @Override protected int getLayoutId() {
+        return R.layout.activity_sign_in;
+    }
 
-  @OnClick(R.id.email_login_button) public void onSignInClick() {
-    presenter.signInClicked(email.getText().toString(), password.getText().toString());
-  }
+    @Override protected void redirectToLogin() {
 
-  @Override
-  public void showSignUp(final String username, final String password) {
-    new AlertDialog.Builder(this).setMessage(R.string.sign_up_confirmation_message)
-        .setPositiveButton(R.string.sign_up_confirmation_ok, new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            presenter.signUp(username, password);
-          }
-        })
-        .setNegativeButton(R.string.sign_up_confirmation_no, null)
-        .create()
-        .show();
-  }
+    }
 
-  @Override
-  public void goToInstagrammersList() {
-    Intent intent = new Intent(this, InstagrammersListActivity.class);
-    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-    startActivity(intent);
-  }
+    @OnClick(R.id.email_login_button) public void onSignInClick() {
+        presenter.signInClicked(email.getText().toString(), password.getText().toString());
+    }
 
-  @Override
-  public void hideLoading() {
+    @Override public void showSignUp(final String username, final String password) {
+        new AlertDialog.Builder(this).setMessage(R.string.sign_up_confirmation_message)
+          .setPositiveButton(R.string.sign_up_confirmation_ok, new DialogInterface.OnClickListener() {
+              @Override public void onClick(DialogInterface dialog, int which) {
+                  presenter.signUp(username, password);
+              }
+          })
+          .setNegativeButton(R.string.sign_up_confirmation_no, null)
+          .create()
+          .show();
+    }
 
-  }
+    @Override public void goToInstagrammersList() {
+        Intent intent = new Intent(this, InstagrammersListActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
 
-  @Override
-  public void showLoading() {
+    @Override public void hideLoading() {
+        /* no-op */
+    }
 
-  }
+    @Override public void showLoading() {
+        /* no-op */
+    }
 }
 
