ea5de94f0cf140caabfb17f2d1b8775212696a75
arturet <arturo.jimenez@fav24.com>
2016-05-18 14:13:34 +0200

    fixed transactions

--- app/src/main/java/com/losextraditables/bu/pictures/view/activity/PicturesActivity.java
+++ app/src/main/java/com/losextraditables/bu/pictures/view/activity/PicturesActivity.java
@@ -86,7 +86,7 @@ public class PicturesActivity extends BuAppCompatActivity
           presenter.saveInstagrammerClicked();
         } else if (menuItemId == R.id.bottom_instagrammers) {
           startActivity(new Intent(context, InstagrammersListActivity.class));
-          overridePendingTransition(R.anim.activity_back_in, R.anim.activity_back_out);
+          overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
           finish();
         }
       }
