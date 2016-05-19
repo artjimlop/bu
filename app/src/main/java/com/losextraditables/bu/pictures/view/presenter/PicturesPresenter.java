11bc43f2b36d7deacf6ec76c7e7391fa18fd1f6a
arturet <arturo.jimenez@fav24.com>
2016-05-19 15:20:21 +0200

    Sing in Activity Changed

--- app/src/main/java/com/losextraditables/bu/pictures/view/presenter/PicturesPresenter.java
+++ app/src/main/java/com/losextraditables/bu/pictures/view/presenter/PicturesPresenter.java
@@ -29,9 +29,9 @@ public class PicturesPresenter extends BuPresenter<PicturesPresenter.View> {
   private final SaveInstagrammerUseCase saveInstagrammerUseCase;
   private final GetPicturesUseCase getPicturesUseCase;
 
-  @Inject public PicturesPresenter(UseCaseHandler useCaseHandler,
-      GetPictureUseCase getPictureUseCase, SavePictureUseCase savePictureUseCase,
-      GetInstagrammerUseCase getInstagrammerUseCase,
+  @Inject
+  public PicturesPresenter(UseCaseHandler useCaseHandler, GetPictureUseCase getPictureUseCase,
+      SavePictureUseCase savePictureUseCase, GetInstagrammerUseCase getInstagrammerUseCase,
       SaveInstagrammerUseCase saveInstagrammerUseCase, GetPicturesUseCase getPicturesUseCase) {
     super(useCaseHandler);
     this.getPictureUseCase = getPictureUseCase;
@@ -50,23 +50,21 @@ public class PicturesPresenter extends BuPresenter<PicturesPresenter.View> {
 
   public void savePicture(final String url, @NonNull final String uid) {
     createUseCaseCall(getPictureUseCase).args(url, uid).onSuccess(new OnSuccessCallback() {
-      @Success
-      public void onPictureSaved(Observable<Picture> pictureObservable) {
-        pictureObservable
-            .subscribeOn(Schedulers.io())
+      @Success public void onPictureSaved(Observable<Picture> pictureObservable) {
+        pictureObservable.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Observer<Picture>() {
-          @Override public void onCompleted() {
-          }
+              @Override public void onCompleted() {
+              }
 
-          @Override public void onError(Throwable e) {
-            getView().showConnectionError();
-          }
+              @Override public void onError(Throwable e) {
+                getView().showConnectionError();
+              }
 
-          @Override public void onNext(Picture picture) {
-            addToUsersPicture(picture, uid);
-          }
-        });
+              @Override public void onNext(Picture picture) {
+                addToUsersPicture(picture, uid);
+              }
+            });
       }
     }).onError(new OnErrorCallback() {
       @Override public boolean onError(Error error) {
@@ -78,8 +76,7 @@ public class PicturesPresenter extends BuPresenter<PicturesPresenter.View> {
 
   private void addToUsersPicture(final Picture picture, String uid) {
     createUseCaseCall(savePictureUseCase).args(picture, uid).onSuccess(new OnSuccessCallback() {
-      @Success
-      public void onPictureSaved(Observable<Void> savePicutreObservable) {
+      @Success public void onPictureSaved(Observable<Void> savePicutreObservable) {
         savePicutreObservable.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Observer<Void>() {
@@ -109,10 +106,8 @@ public class PicturesPresenter extends BuPresenter<PicturesPresenter.View> {
 
   public void saveUser(String url, final String uid) {
     createUseCaseCall(getInstagrammerUseCase).args(url, uid).onSuccess(new OnSuccessCallback() {
-      @Success
-      public void onPictureSaved(Observable<Instagrammer> instagrammerObservable) {
-        instagrammerObservable
-            .subscribeOn(Schedulers.io())
+      @Success public void onPictureSaved(Observable<Instagrammer> instagrammerObservable) {
+        instagrammerObservable.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Observer<Instagrammer>() {
               @Override public void onCompleted() {
@@ -136,70 +131,68 @@ public class PicturesPresenter extends BuPresenter<PicturesPresenter.View> {
   }
 
   private void addToUsersInstagrammers(Instagrammer instagrammer, String uid) {
-    createUseCaseCall(saveInstagrammerUseCase).args(instagrammer, uid).onSuccess(new OnSuccessCallback() {
-      @Success
-      public void onPictureSaved(Observable<Void> saveInstagrammerObservable) {
-        saveInstagrammerObservable.subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(new Observer<Void>() {
-              @Override public void onCompleted() {
-                getView().showSavedInstagrammer();
-              }
-
-              @Override public void onError(Throwable e) {
-
-              }
-
-              @Override public void onNext(Void aVoid) {
-
-              }
-            });
-      }
-    }).onError(new OnErrorCallback() {
-      @Override public boolean onError(Error error) {
-        return false;
-      }
-    }).execute();
-  }
-
-  public void loadSavedPictures(String uid) {
-    getView().showLoading();
-    createUseCaseCall(getPicturesUseCase).args(uid)
+    createUseCaseCall(saveInstagrammerUseCase).args(instagrammer, uid)
         .onSuccess(new OnSuccessCallback() {
-          @Success
-          public void onInstagrammersLoaded(Observable<List<Picture>> observable) {
-            observable.subscribeOn(Schedulers.io())
+          @Success public void onPictureSaved(Observable<Void> saveInstagrammerObservable) {
+            saveInstagrammerObservable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<Picture>>() {
+                .subscribe(new Observer<Void>() {
                   @Override public void onCompleted() {
-                    getView().hideLoading();
-                    //TODO show no pictures text
+                    getView().showSavedInstagrammer();
                   }
 
                   @Override public void onError(Throwable e) {
-                    getView().hideLoading();
-                    getView().showConnectionError();
+
                   }
 
-                  @Override public void onNext(List<Picture> pictures) {
-                    //TODO MODEL List<InstagrammerModel> instagrammerModels = mapper.mapList(instagrammers);
-                    ArrayList<String> urls = new ArrayList<>(pictures.size());
-                    for (Picture picture : pictures) {
-                      urls.add(picture.getUrl());
-                    }
-                    getView().hideLoading();
-                    getView().showSavedPictures(urls);
+                  @Override public void onNext(Void aVoid) {
+
                   }
                 });
           }
         })
         .onError(new OnErrorCallback() {
           @Override public boolean onError(Error error) {
-            getView().hideLoading();
             return false;
           }
         })
         .execute();
+  }
+
+  public void loadSavedPictures(String uid) {
+    getView().showLoading();
+    createUseCaseCall(getPicturesUseCase).args(uid).onSuccess(new OnSuccessCallback() {
+      @Success public void onInstagrammersLoaded(Observable<List<Picture>> observable) {
+        observable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Observer<List<Picture>>() {
+              @Override public void onCompleted() {
+                getView().hideLoading();
+                //TODO show no pictures text
+              }
+
+              @Override public void onError(Throwable e) {
+                getView().hideLoading();
+                getView().showConnectionError();
+              }
+
+              @Override public void onNext(List<Picture> pictures) {
+                //TODO MODEL List<InstagrammerModel> instagrammerModels = mapper.mapList(instagrammers);
+                ArrayList<String> urls = new ArrayList<>(pictures.size());
+                for (Picture picture : pictures) {
+                  urls.add(picture.getUrl());
+                }
+                getView().hideLoading();
+                getView().showSavedPictures(urls);
+              }
+            });
+      }
+    }).onError(new OnErrorCallback() {
+      @Override public boolean onError(Error error) {
+        getView().hideLoading();
+        return false;
+      }
+    }).execute();
   }
 
   public interface View extends BuPresenter.View {
@@ -210,8 +203,7 @@ public class PicturesPresenter extends BuPresenter<PicturesPresenter.View> {
     void showSaveInstagrammerDialog();
 
     void showSavedInstagrammer();
-    
+
     void showSavedPictures(ArrayList<String> urls);
   }
-
 }
--- app/src/main/java/com/losextraditables/bu/utils/BlurTransform.java
+++ app/src/main/java/com/losextraditables/bu/utils/BlurTransform.java
@@ -6,22 +6,22 @@ import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
-
 import com.squareup.picasso.Transformation;
 
 public class BlurTransform implements Transformation {
 
   RenderScript rs;
+
   public BlurTransform(Context context) {
     super();
     rs = RenderScript.create(context);
   }
 
-  @Override
-  public Bitmap transform(Bitmap bitmap) {
+  @Override public Bitmap transform(Bitmap bitmap) {
     Bitmap blurredBitmap = Bitmap.createBitmap(bitmap);
 
-    Allocation input = Allocation.createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_FULL, Allocation.USAGE_SHARED);
+    Allocation input = Allocation.createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_FULL,
+        Allocation.USAGE_SHARED);
     Allocation output = Allocation.createTyped(rs, input.getType());
 
     ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
@@ -35,9 +35,7 @@ public class BlurTransform implements Transformation {
     return blurredBitmap;
   }
 
-  @Override
-  public String key() {
+  @Override public String key() {
     return "blur";
   }
-
 }
