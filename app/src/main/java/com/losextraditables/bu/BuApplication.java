12ca60858587792f3ff839d09925f8e19f0cedde
Arturo <artjimlop@gmail.com>
2016-05-24 08:16:45 +0200

    Firebase autologin when token expires
    Done in Application, should be extracted to another module

--- app/src/main/java/com/losextraditables/bu/BuApplication.java
+++ app/src/main/java/com/losextraditables/bu/BuApplication.java
@@ -1,14 +1,21 @@
 package com.losextraditables.bu;
 
 import com.crashlytics.android.Crashlytics;
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.karumi.rosie.application.RosieApplication;
+import com.losextraditables.bu.utils.InstagramSession;
 import dagger.ObjectGraph;
 import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 import java.util.List;
+import javax.inject.Inject;
 
 public class BuApplication extends RosieApplication {
+
+  @Inject InstagramSession session;
+
   private ObjectGraph fakeObjectGraph;
 
   @Override
@@ -16,6 +23,28 @@ public class BuApplication extends RosieApplication {
     super.onCreate();
     Fabric.with(this, new Crashlytics());
     Firebase.setAndroidContext(this);
+
+    final Firebase ref = new Firebase("https://buandroid.firebaseio.com");
+
+    ref.addAuthStateListener(new Firebase.AuthStateListener() {
+      @Override
+      public void onAuthStateChanged(AuthData authData) {
+        if (authData == null) {
+          Firebase.AuthResultHandler authResultHandler = new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+              // Authenticated successfully with payload authData
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+              Crashlytics.log(firebaseError.getMessage());
+            }
+          };
+          ref.authWithCustomToken(session.getUid(getApplicationContext()), authResultHandler);
+        }
+      }
+    });
   }
 
   @Override protected List<Object> getApplicationModules() {
