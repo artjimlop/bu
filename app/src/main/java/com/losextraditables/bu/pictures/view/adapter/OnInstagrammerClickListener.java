f881f59a0ef2ecb1ad952ad32e9510c3a7fb8642
Arturet <artjimlop@gmail.com>
2016-07-14 15:36:14 +0200

    users can remove instagrammers

---
+++ app/src/main/java/com/losextraditables/bu/pictures/view/adapter/OnInstagrammerClickListener.java
@@ -0,0 +1,7 @@
+package com.losextraditables.bu.pictures.view.adapter;
+
+import android.view.View;
+
+public interface OnInstagrammerClickListener {
+  void onItemLongClick(View view, String username);
+}
