4d4821f0e0d2cc6ffc4fd87abb2591e7e64d9c17
jmartinez1360 <mrjem1360@gmail.com>
2016-07-29 13:00:45 +0200

    Se incluye un viewPager parallax para que la galeria quede más mona (Ramsy style)

--- app/src/main/java/com/losextraditables/bu/widgets/CustomViewPager.java
+++ app/src/main/java/com/losextraditables/bu/widgets/CustomViewPager.java
@@ -1,11 +1,11 @@
 package com.losextraditables.bu.widgets;
 
 import android.content.Context;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import cn.yangbingqiang.android.parallaxviewpager.ParallaxViewPager;
 
-public class CustomViewPager extends ViewPager {
+public class CustomViewPager extends ParallaxViewPager {
   public CustomViewPager(Context context) {
     super(context);
   }
